64412c033d76e10e2e9c91571021e12bf447b2ca
OSS0302 <tkdtn3264@gmail.com>
2024-02-17 18:31:25 +0900

    api 추가

---
+++ lib/data/data_source/piabay_api.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../../key.dart';
+class PixabayApi {
+  Future<Map<String, dynamic>> getImageResult(String query) async {
+      final response = await http.get(Uri.parse('https://pixabay.com/api/?key=$key&q=$query&image_type=photo'));
+      return jsonDecode(response.body);
+  }
+}
