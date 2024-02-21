d2f6396f643861512be0a06466ad59058e661c4c
OSS0302 <tkdtn3264@gmail.com>
2024-02-21 18:10:28 +0900

    03 mvvm + provider + getit +go router  적용한 이미지 앱

---
+++ lib/routes.dart
@@ -0,0 +1,17 @@
+import 'package:go_router/go_router.dart';
+import 'package:image_search_app/data/repository/image_item_repository_impl.dart';
+import 'package:image_search_app/ui/main/main_screen.dart';
+import 'package:image_search_app/ui/main/main_view_model.dart';
+import 'package:provider/provider.dart';
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => ChangeNotifierProvider(
+        create: (_) => MainViewModel(repository: ImageItemRepositoryImpl()),
+        child: MainScreen(),
+      ),
+    ),
+  ],
+);
