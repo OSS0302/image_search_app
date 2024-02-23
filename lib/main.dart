fae51d62ea5e96497587a6fdaba94d7ed3c57c8a
OSS0302 <tkdtn3264@gmail.com>
2024-02-23 11:57:26 +0900

    03 mvvm + provider +getit+ gorouter 이미지 앱

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:image_search_app/data/repository/image_item_repository_impl.dart';
+import 'package:image_search_app/rotures.dart';
 import 'package:image_search_app/ui/main/main_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -18,16 +19,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ChangeNotifierProvider(
-        create: (_) => MainViewModel(repository: ImageItemRepositoryImpl()),
-        child: MainScreen(),
-      ),
+
+
 
     );
   }
