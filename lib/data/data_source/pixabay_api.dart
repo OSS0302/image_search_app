69ad81f3bb27e7b6524728b85b78f6b2833cd356
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-02-24 22:29:29 +0900

    00_future_builder

--- lib/data/data_source/pixabay_api.dart
+++ lib/data/data_source/pixabay_api.dart
@@ -1,12 +1,10 @@
 import 'dart:convert';
 
-import 'package:image_search_app/key.dart';
-
 import '../dto/image_dto.dart';
 import 'package:http/http.dart' as http;
-class PixabayApi {
-  Future<ImageDto> getResult(String query ) async{
+class PixabayApi{
+  Future<ImageDto> getImageData(String query) async {
     final response = await http.get(Uri.parse('https://pixabay.com/api/?key=10711147-dc41758b93b263957026bdadb&q=$query&imagetype=photo'));
-  return ImageDto.fromJson(jsonDecode(response.body));
-
+    return ImageDto.fromJson(jsonDecode(response.body));
+  }
 }
--- lib/data/mapper/image_mapper.dart
+++ lib/data/mapper/image_mapper.dart
@@ -1,9 +1,8 @@
 import 'package:image_search_app/data/dto/image_dto.dart';
-import 'package:image_search_app/domain/model/image_item.dart';
+import 'package:image_search_app/domain/model/image_items.dart';
 
-
-extension DtoToModel on Hits {
-ImageItem toImageItem(){
-  return ImageItem(imageUrl: previewURL ?? '', tags: tags ?? '', id: id as int);
-}
+extension DtoToModel on Hits{
+  ImageItems toImageItems(){
+    return ImageItems(imageUrl: previewURL ?? '', tags: tags ?? '', id: id as int);
+  }
 }
