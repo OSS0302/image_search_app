e5c2ac3bb8db978124007e813e4854ff58f9804a
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-02-26 22:03:07 +0900

    03 getit+ provier

--- lib/presentation/main/main_view_model.dart
+++ lib/presentation/main/main_view_model.dart
@@ -3,24 +3,30 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:image_search_app/data/repository/image_item_repository_impl.dart';
 import 'package:image_search_app/domain/model/image_item.dart';
+import 'package:image_search_app/domain/repository/image_item_repository.dart';
 
 class MainViewModel extends ChangeNotifier {
-  final repository = ImageItemRepositoryImpl();
-  List<ImageItem> _imageItems = [];
-  bool isLoading = false;
-  List<ImageItem> get  imageItem => List.unmodifiable(_imageItems);
+  final ImageItemRepository _repository;
+   MainViewModel({
+    required ImageItemRepository repository,
+  }) : _repository = repository;
 
-  final _loadController = StreamController<bool>();
-  Stream<bool>get isLoadingStream => _loadController.stream;
+  List<ImageItem> _imageItems = [];
+  bool _isLoading = false;
+  List<ImageItem> get  imageItem => List.unmodifiable(_imageItems);
+  bool get isLoading =>_isLoading;
+
+
 
 
 
   Future<void> fetchImage(String query) async{
-   _loadController.add(true);
-
-   _imageItems = await repository.getImageResult(query);
-   _loadController.add(false);
+    _isLoading =true;
+    notifyListeners();
+   _imageItems = await _repository.getImageResult(query);
+    _isLoading =false;
    notifyListeners();
   }
+
 
 }
