0ef3610c713d659604664afdbf73793d5e057a71
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-02-29 20:52:42 +0900

    스낵바 추가 하기

--- lib/presentation/main/main_view_model.dart
+++ lib/presentation/main/main_view_model.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:image_search_app/domain/repository/image_item_repository.dart';
+import 'package:image_search_app/presentation/main/main_event.dart';
 import 'package:image_search_app/presentation/main/main_state.dart';
 
 import '../../core/result.dart';
@@ -13,6 +14,9 @@ class MainViewModel extends ChangeNotifier {
   MainViewModel({
     required ImageItemRepository repository,
   }) : _repository = repository;
+
+  final _eventController = StreamController<MainEvent>();
+  Stream<MainEvent> get eventStream => _eventController.stream;
 
   MainState _state = MainState(imageItems: [], isLoading: false);
 
@@ -30,8 +34,13 @@ class MainViewModel extends ChangeNotifier {
           isLoading: false,
           imageItems: result.data.toList(),
         );
+        notifyListeners();
+        _eventController.add(MainEvent.showSnackBar('성공!'));
       case Error<List<ImageItem>>():
-          print('에러다');
+          _state = state.copyWith(isLoading:  false);
+          notifyListeners();
+
+        _eventController.add(MainEvent.showSnackBar(result.e.toString()));
       case Loading<List<ImageItem>>():
          print('로딩');
     }
