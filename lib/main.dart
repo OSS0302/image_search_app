efaf9ca6eb025ab2664485da8d4950fda257e9cb
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-03-13 15:50:37 +0900

    03_ mvvm+ provider + getIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:image_search_app/data/di/di_setup.dart';
+import 'package:image_search_app/data/repository/image_item_repository.dart';
 import 'package:image_search_app/ui/main/main_screen.dart';
+import 'package:image_search_app/ui/main/main_view_model.dart';
+import 'package:provider/provider.dart';
 
 
 
 void main() {
+  diSetUp();
   runApp(const MyApp());
 
 }
@@ -19,7 +24,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MainScreen(),
+    home : ChangeNotifierProvider(
+       create: (_) => MainViewModel(repository: ImageItemRepositoryImpl()),
+       child: MainScreen(),
+    )
     );
   }
 }
