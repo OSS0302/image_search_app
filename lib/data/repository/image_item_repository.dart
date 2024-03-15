a6063b447cfb648cbd721e3a3e33dfdf7419a0c1
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-03-15 19:33:58 +0900

    08_ mvvm+ provider + getIt + go router + state(freezed) +result  적용한 이미지 앱

--- lib/data/repository/image_item_repository.dart
+++ lib/data/repository/image_item_repository.dart
@@ -1,37 +1,24 @@
 import 'package:image_search_app/data/data_source/pixabay_api.dart';
 import 'package:image_search_app/data/mapper/image_mapper.dart';
 
+import '../../core/result.dart';
 import '../model/image_item.dart';
 
 abstract interface class ImageItemRepository {
-  Future<List<ImageItem>> getSearchImage(String query);
+  Future<Result<List<ImageItem>>> getSearchImage(String query);
 }
 
 class ImageItemRepositoryImpl implements ImageItemRepository {
   final _api = PixabayApi();
 
   @override
-  Future<List<ImageItem>> getSearchImage(String query) async {
+  Future<Result<List<ImageItem>>> getSearchImage(String query) async {
     final dto = await _api.getImageResult(query);
     if (dto.hits == null) {
-      return [];
+      return Result.success([]);
     }
-    return dto.hits!.map((e) => e.toImageItem()).toList();
+    return Result.success( dto.hits!.map((e) => e.toImageItem()).toList());
   }
 }
 
-class MockImageItemRepositoryImpl implements ImageItemRepository {
-  @override
-  Future<List<ImageItem>> getSearchImage(String query) async{
-   await Future.delayed(const Duration(seconds: 2));
-    if (query == 'apple') {
-      return[
-        ImageItem(imageUrl: "https://cdn.pixabay.com/photo/2016/01/05/13/58/apple-1122537_150.jpg", tags:  '', id: 1)
-      ];
-    }else{
-      return[
-        ImageItem(imageUrl: "https://cdn.pixabay.com/photo/2016/01/05/13/58/apple-1122537_150.jpg", tags:  '', id: 2)
-      ];
-    }
-  }
-}
+
