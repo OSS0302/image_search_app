62c608218fd8e2ff496c34adb4b3f3f22f02a0d4
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-03-30 16:20:13 +0900

    08 mvvm + provider + getIt + router + state + freezed + result + showSnackBar

--- lib/presentation/main/pixabay_view_model.dart
+++ lib/presentation/main/pixabay_view_model.dart
@@ -4,27 +4,40 @@ import 'package:flutter/material.dart';
 import 'package:image_search_app/core/result.dart';
 import 'package:image_search_app/data/model/pixabay_item.dart';
 import 'package:image_search_app/data/repository/pixabay_repository.dart';
+import 'package:image_search_app/presentation/main/pixabay_event.dart';
 import 'package:image_search_app/presentation/main/pixabay_state.dart';
 
 class PixabayViewModel extends ChangeNotifier {
   final PixabayRepository _pixabayRepository;
 
-   PixabayViewModel({
+  PixabayViewModel({
     required PixabayRepository pixabayRepository,
   }) : _pixabayRepository = pixabayRepository;
 
-   PixabayState _state =  PixabayState(imageItems: List.unmodifiable([]), isLoading: false);
+  PixabayState _state =
+      PixabayState(imageItems: List.unmodifiable([]), isLoading: false);
 
-   PixabayState get state => _state;
+  PixabayState get state => _state;
+
+  final _eventController = StreamController<PixabayEvent>();
+
+  Stream<PixabayEvent> get eventStream => _eventController.stream;
 
   Future<void> fetchImage(String query) async {
-
     final result = await _pixabayRepository.getImageItems(query);
-    switch(result){
+    switch (result) {
       case Success<List<PixabayItem>>():
-        _state = state.copyWith(isLoading: true, imageItems: result.data.toList());
+        _state = state.copyWith(
+          isLoading: true,
+          imageItems: result.data.toList(),
+        );
+        notifyListeners();
+        _eventController.add(const PixabayEvent.showSnackBar('성공'));
       case Error<List<PixabayItem>>():
-        _state = state.copyWith(isLoading: false);
+        _state = state.copyWith(
+          isLoading: false,
+        );
+        _eventController.add(PixabayEvent.showSnackBar(result.e.toString()));
     }
   }
 }
