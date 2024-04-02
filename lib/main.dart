8246373e66ebf2c35a43d11a53d5492a11de6662
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-04-02 16:14:25 +0900

    04. mvvm + provider + getIt + router

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:image_search_app/data/repository/pixabay_repository.dart';
+import 'package:image_search_app/di/di_setup.dart';
 import 'package:image_search_app/routes.dart';
-import 'package:image_search_app/ui/pixabay/pixabay_screen.dart';
-import 'package:image_search_app/ui/pixabay/pixabay_view_model.dart';
-import 'package:provider/provider.dart';
+
 
 
 void main() {
+  diSetUp();
   runApp(const MyApp());
 }
 
@@ -15,16 +14,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ChangeNotifierProvider(
-        create: (_) => PixabayViewModel(repository: PixabayRepositoryImpl()),
-        child: PixabayScreen(),
-      )
     );
   }
 }
