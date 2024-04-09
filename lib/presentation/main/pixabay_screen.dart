3c6716f47c580ebf06bc5cce944ad414a70df28a
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-04-09 20:11:36 +0900

    02 mvvm + stream

--- lib/presentation/main/pixabay_screen.dart
+++ lib/presentation/main/pixabay_screen.dart
@@ -25,58 +25,69 @@ class _PixabayScreenState extends State<PixabayScreen> {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              TextField(
-                controller: imageSearchController,
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        width: 2,
-                        color: Colors.pinkAccent,
-                      ),
+            TextField(
+            controller: imageSearchController,
+            decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    width: 2,
+                    color: Colors.pinkAccent,
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    width: 2,
+                    color: Colors.pinkAccent,
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                hintText: '이미지 검색하세요',
+                suffixIcon: IconButton(
+                  onPressed: () async {
+                    await pixbayViewModel
+                        .fetchImage(imageSearchController.text);
+                    setState(() {});
+                  },
+                  icon: const Icon(
+                    Icons.search,
+                    color: Colors.pinkAccent,
+                  ),
+                )),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          StreamBuilder<bool>(
+            initialData: false,
+            stream: pixbayViewModel.loadingStream,
+            builder: (context, snapshot) {
+              if(snapshot.data! == true){
+                return Center(child: CircularProgressIndicator(),);
+              }
+              return Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    mainAxisSpacing: 22,
+                    crossAxisSpacing: 22,
+                  ),
+                  itemCount: pixbayViewModel.imageItem.length,
+                  itemBuilder: (context, index) {
+                    final imageItem = pixbayViewModel.imageItem[index];
+                    return ClipRRect(
                       borderRadius: BorderRadius.circular(20),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        width: 2,
-                        color: Colors.pinkAccent,
+                      child: Image.network(
+                        imageItem.imageUrl,
+                        fit: BoxFit.cover,
                       ),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    hintText: '이미지 검색하세요',
-                    suffixIcon: IconButton(
-                      onPressed: () async {
-                       await pixbayViewModel.fetchImage(imageSearchController.text);
-                        setState(() {});
-                      },
-                      icon: const Icon(
-                        Icons.search,
-                        color: Colors.pinkAccent,
-                      ),
-                    )),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-             pixbayViewModel.isLoading ?  Center(child: CircularProgressIndicator(),)
-                   : Expanded(
-                    child: GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4,
-                        mainAxisSpacing: 22,
-                        crossAxisSpacing: 22,
-                      ),
-                      itemCount: pixbayViewModel.imageItem.length,
-                      itemBuilder: (context, index) {
-                        final imageItem = pixbayViewModel.imageItem[index];
-                        return ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.network(imageItem.imageUrl,fit: BoxFit.cover,),
-                        );
-                      },
-                    ),
+                    );
+                  },
+                ),
+              );
+            },
+          ),
 
-
-              ),
             ],
           ),
         ),
--- lib/presentation/main/pixabay_view_model.dart
+++ lib/presentation/main/pixabay_view_model.dart
@@ -1,18 +1,26 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:image_search_app/data/model/pixabay_item.dart';
 import 'package:image_search_app/data/repository/pixabay_repository.dart';
 
 class PixabayViewModel extends ChangeNotifier {
   final _repository = PixabayRepositoryImpl();
-  List<PixabayItem> imageItem = [];
+  List<PixabayItem> _imageItem = [];
+  List<PixabayItem> get imageItem  => List.unmodifiable(_imageItem);
+
   bool isLoading = false;
 
+  final _loadingController = StreamController<bool>();
+  Stream<bool> get loadingStream => _loadingController.stream;
+
+
   Future<void> fetchImage(String query) async {
-    isLoading = true;
+    _loadingController.add(true);
     notifyListeners();
 
-    imageItem = await _repository.getResult(query);
-    isLoading = false;
+    _imageItem = await _repository.getResult(query);
+    _loadingController.add(false);
     notifyListeners();
   }
 }
