954dbfcf2fbc571eff622b87114d54d0c2e6c9f8
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-04-15 21:03:48 +0900

    09 Clean Architecture + provider + getIt +router + result + freezed +snackBar + ShowSnackBar

--- lib/data/repository/image_repository_impl.dart
+++ lib/data/repository/image_repository_impl.dart
@@ -2,7 +2,7 @@ import 'package:image_search_app/data/data_source/pixabay_api.dart';
 import 'package:image_search_app/data/mapper/image_mapper.dart';
 
 import '../../core/result.dart';
-import '../../model/image_item.dart';
+import '../../domain/model/image_item.dart';
 
 abstract interface class ImageRepository {
   Future<Result<List<ImageItem>>> getfetchImage(String query);
