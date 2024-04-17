9fd9a33f0cd6d9403c57df08bd90be58f018726e
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-04-17 13:50:38 +0900

    04 mvvm + provider + getIt + router

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,1 +1,18 @@
+import 'package:go_router/go_router.dart';
+import 'package:image_search_app/presentation/main/main_screen.dart';
+import 'package:image_search_app/presentation/main/main_view_model.dart';
+import 'package:provider/provider.dart';
 
+import 'data/repository/image_repository_impl.dart';
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => ChangeNotifierProvider(
+        create: (_) => MainViewModel(repository: ImageRepositoryImpl()),
+        child: MainScreen(),
+      ),
+    ),
+  ],
+);
