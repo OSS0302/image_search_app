b5af5610b03ff2b70a44ae6dbd8be6ab1aee1f7c
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-04-18 14:48:20 +0900

    08 mvvm + provider + getIt + router + state + result + freezed (snackBar + showDialog)

--- lib/presentation/pixabay/pixabay_screen.dart
+++ lib/presentation/pixabay/pixabay_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:image_search_app/presentation/pixabay/pixabay_event.dart';
 import 'package:image_search_app/presentation/pixabay/pixabay_view_model.dart';
 import 'package:image_search_app/presentation/widget/pixabay_widget.dart';
 import 'package:provider/provider.dart';
@@ -13,6 +17,53 @@ class PixabayScreen extends StatefulWidget {
 class _PixabayScreenState extends State<PixabayScreen> {
   final imageSearchController = TextEditingController();
 
+  StreamSubscription<PixabayEvent>? subscription;
+
+  @override
+  void initState() {
+    Future.microtask(() {
+      subscription =
+          context.read<PixabayViewModel>().eventStream.listen((event) {
+        switch (event) {
+          case ShowSnackBar():
+            final snackBar = SnackBar(content: Text(event.message));
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          case ShowDialog():
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text('이미지 검색앱'),
+                  content: Text('이미지 가져오기 완료'),
+                  actions: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.blue,
+                      ),
+                      child: TextButton(
+                        onPressed: () {
+                          context.pop();
+                        },
+                        child: Text('확인'),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            );
+        }
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    imageSearchController.dispose();
+    subscription?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,10 +98,8 @@ class _PixabayScreenState extends State<PixabayScreen> {
                   hintText: '이미지를 검색하세요',
                   suffixIcon: IconButton(
                     onPressed: () async {
-                       await pixabayViewModel
+                      await pixabayViewModel
                           .fetchImage(imageSearchController.text);
-
-
                       setState(() {});
                     },
                     icon: const Icon(
@@ -63,20 +112,24 @@ class _PixabayScreenState extends State<PixabayScreen> {
               const SizedBox(
                 height: 24,
               ),
-              state.isLoading ? const Center(child: CircularProgressIndicator(),)
-              : Expanded(
-                child: GridView.builder(
-                  itemCount: state.pixabayItem.length,
-                  itemBuilder: (context, index) {
-                    final pixabayItems = state.pixabayItem[index];
-                    return PixabayWidget(pixabayItems: pixabayItems);
-                  },
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4,
-                      mainAxisSpacing: 32,
-                      crossAxisSpacing: 32),
-                ),
-              ),
+              state.isLoading
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : Expanded(
+                      child: GridView.builder(
+                        itemCount: state.pixabayItem.length,
+                        itemBuilder: (context, index) {
+                          final pixabayItems = state.pixabayItem[index];
+                          return PixabayWidget(pixabayItems: pixabayItems);
+                        },
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 4,
+                                mainAxisSpacing: 32,
+                                crossAxisSpacing: 32),
+                      ),
+                    ),
             ],
           ),
         ),
--- lib/presentation/pixabay/pixabay_view_model.dart
+++ lib/presentation/pixabay/pixabay_view_model.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:image_search_app/core/result.dart';
 import 'package:image_search_app/data/model/pixabay_item.dart';
+import 'package:image_search_app/presentation/pixabay/pixabay_event.dart';
 import 'package:image_search_app/presentation/pixabay/pixabay_state.dart';
 
 import '../../data/repository/pixabay_repository.dart';
@@ -19,6 +20,9 @@ class PixabayViewModel extends ChangeNotifier {
 
   PixabayState get state => _state;
 
+  final eventController = StreamController<PixabayEvent>();
+  Stream<PixabayEvent> get eventStream => eventController.stream;
+
   Future<void> fetchImage(String query) async {
     final result = (await _repository.getfetchImage(query));
     _state = state.copyWith(isLoading: true);
@@ -31,6 +35,9 @@ class PixabayViewModel extends ChangeNotifier {
         isLoading: false,
          pixabayItem: result.data,
        );
+       notifyListeners();
+       eventController.add(const PixabayEvent.showSnackBar('데이터 가져오기 완료'));
+       eventController.add(const PixabayEvent.showDialog('데이터 가져오기 완료'));
       case Error<List<PixabayItem>>():
         print('에얼');
     }
