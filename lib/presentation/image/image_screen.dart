6405d6048103c47b751ec9056b89b90014ba8b97
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-04-19 18:05:59 +0900

    08 mvvm + getIt + provider + router + state + freezed + result + sanckBar + showDialog

--- lib/presentation/image/image_screen.dart
+++ lib/presentation/image/image_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:image_search_app/presentation/image/image_view_model.dart';
 import 'package:provider/provider.dart';
+
+import 'image_event.dart';
 
 class ImageScreen extends StatefulWidget {
   const ImageScreen({super.key});
@@ -11,6 +16,34 @@ class ImageScreen extends StatefulWidget {
 
 class _ImageScreenState extends State<ImageScreen> {
   final textEditingController = TextEditingController();
+   StreamSubscription<ImageEvent>?  subscription;
+
+  @override
+  void initState() {
+    Future.microtask(() {
+     subscription = context.read<ImageViewModel>().eventStream.listen((event) {
+       switch(event){
+
+         case ShowSnackBar():
+          final snackBar = SnackBar(content: Text(event.message));
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+         case ShowDialog():
+            showDialog(context: context, builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('이미지 검색앱'),
+                content: Text('데이터 가져오기'),
+                actions: [
+                  TextButton(onPressed: () {
+                    context.pop();
+                  }, child: Text('확인'))
+                ],
+              );
+            });
+       }
+     });
+    });
+    super.initState();
+  }
 
 
   @override
--- lib/presentation/image/image_view_model.dart
+++ lib/presentation/image/image_view_model.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:image_search_app/core/result.dart';
 import 'package:image_search_app/data/repository/image_repository_impl.dart';
+import 'package:image_search_app/presentation/image/image_event.dart';
 
 import '../../data/model/image_item.dart';
 import '../../data/repository/image_repository.dart';
@@ -20,6 +21,9 @@ class ImageViewModel extends ChangeNotifier {
 
   ImageState get state => _state;
 
+  final _eventController  = StreamController<ImageEvent>();
+  Stream<ImageEvent> get eventStream => _eventController.stream;
+
   Future<void> searchImage(String query) async {
     _state = state.copyWith(
       isLoading: true,
@@ -34,6 +38,8 @@ class ImageViewModel extends ChangeNotifier {
           imageItem:  result.data,
         );
         notifyListeners();
+        _eventController.add(const ImageEvent.showSnackBar('데이터가져오기 '));
+        _eventController.add(const ImageEvent.showDialog('다이얼로그 생성'));
 
       case Error<List<ImageItem>>():
         _state = state.copyWith(
