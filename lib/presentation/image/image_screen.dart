ad8cfdea720e98b54f6806afa787a21d28d34ecb
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-04-24 21:37:55 +0900

    08 mvvm + provider + getIt + router + state + result + freezed

--- lib/presentation/image/image_screen.dart
+++ lib/presentation/image/image_screen.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:image_search_app/presentation/image/image_view_model.dart';
 import 'package:provider/provider.dart';
 
 import '../widget/image_widget.dart';
+import 'image_event.dart';
 
 class ImageScreen extends StatefulWidget {
   const ImageScreen({super.key});
@@ -13,6 +17,43 @@ class ImageScreen extends StatefulWidget {
 
 class _ImageScreenState extends State<ImageScreen> {
   final imageSearchController = TextEditingController();
+  StreamSubscription<ImageEvent>? subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      subscription = context.read<ImageViewModel>().eventStream.listen((event) {
+        switch (event) {
+          case ShowSnackBar():
+            final snackBar = SnackBar(content: Text(event.message));
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          case ShowDialog():
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('이미지 검색앱'),
+                    content: Text('이미지 가져오기 완료'),
+                    actions: [
+                      TextButton(onPressed: () {
+                        context.pop();
+                      }, child: Text('확인'))
+                    ],
+                  );
+                });
+        }
+      });
+    });
+
+  }
+
+  @override
+  void dispose() {
+    subscription?.cancel();
+    imageSearchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,8 +91,7 @@ class _ImageScreenState extends State<ImageScreen> {
                     onPressed: () async {
                       await imageViewModel
                           .fetchImage(imageSearchController.text);
-                     setState(() {});
-
+                      setState(() {});
                     },
                   ),
                 ),
--- lib/presentation/image/image_view_model.dart
+++ lib/presentation/image/image_view_model.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:image_search_app/core/result.dart';
 import 'package:image_search_app/data/model/image_item.dart';
+import 'package:image_search_app/presentation/image/image_event.dart';
 import '../../data/repository/image_repository.dart';
 import 'image_state.dart';
 
@@ -15,6 +18,10 @@ class ImageViewModel extends ChangeNotifier {
 
   ImageState get state => _state;
 
+  final _eventController = StreamController<ImageEvent>();
+
+  Stream<ImageEvent> get eventStream => _eventController.stream;
+
 
   Future<void> fetchImage(String query) async{
     final result = await _repository.getImageItem(query);
@@ -27,10 +34,18 @@ class ImageViewModel extends ChangeNotifier {
       case Success<List<ImageItem>>():
         _state = state.copyWith(
           isLoading: false,
-          imageItems: result.data
+          imageItems: result.data.toList()
         );
+        notifyListeners();
+        _eventController.add(const ImageEvent.showSnackBar('데이터 가져오기'));
+        _eventController.add(const ImageEvent.showDialog('다이얼로그'));
       case Error<List<ImageItem>>():
-        print('');
+        _state = state.copyWith(
+            isLoading: false,
+        );
+        notifyListeners();
+         _eventController.add(ImageEvent.showDialog(result.e.toString()));
     }
+
   }
 }
