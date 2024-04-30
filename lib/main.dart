09d4c98f5ac159072b12e844ffbbb914e009a211
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-04-30 17:58:44 +0900

    04 mvvm + provider + getIt + router

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:image_search_app/data/repository/image_repository_impl.dart';
+import 'package:image_search_app/di/di_setup.dart';
 import 'package:image_search_app/presentation/image/image_screen.dart';
 import 'package:image_search_app/presentation/image/image_view_model.dart';
+import 'package:image_search_app/routes.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  diSetup();
   runApp(const MyApp());
 }
 
@@ -13,16 +16,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ChangeNotifierProvider(
-        create: (_) => ImageViewModel(repository: ImageRepositoryImpl()),
-        child: ImageScreen(),
-      )
     );
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,3 +1,19 @@
 import 'package:go_router/go_router.dart';
+import 'package:image_search_app/di/di_setup.dart';
+import 'package:image_search_app/presentation/image/image_screen.dart';
+import 'package:image_search_app/presentation/image/image_view_model.dart';
 import 'package:provider/provider.dart';
 
+import 'data/repository/image_repository_impl.dart';
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => ChangeNotifierProvider(
+        create: (_) => getIt<ImageViewModel>(),
+        child: ImageScreen(),
+      ),
+    ),
+  ],
+);
