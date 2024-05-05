7b1df9115bae803a7887d4bb6f9451442f3252ff
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-05-05 20:19:12 +0900

    00 FutureBuilder

--- lib/domain/repository/pixabay_repository.dart
+++
@@ -1,7 +0,0 @@
-
-import '../../core/result.dart';
-import '../model/pixabay_item.dart';
-
-abstract interface class PixabayRepository {
-  Future<Result<List<PixabayItem>>> getImageItems(String query);
-}
---
+++ lib/presentation/image/image_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:image_search_app/data/repository/image_repository_impl.dart';
+import 'package:image_search_app/presentation/widget/image_widget.dart';
+
+class ImageScreen extends StatefulWidget {
+  const ImageScreen({super.key});
+
+  @override
+  State<ImageScreen> createState() => _ImageScreenState();
+}
+
+class _ImageScreenState extends State<ImageScreen> {
+  final textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('검색 이미지 앱 '),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: textEditingController,
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.ads_click_rounded),
+                    onPressed: () {
+                      setState(() {});
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              FutureBuilder(
+                  future: ImageRepositoryImpl()
+                      .getResult(textEditingController.text),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    final imageItem = snapshot.data!;
+                    return Expanded(
+                        child: GridView.builder(
+                      itemCount: imageItem.length,
+                      itemBuilder: (context, index) {
+                        final imageItems = imageItem[index];
+                        return ImageWidget(imageItems: imageItems);
+                      },
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 4,
+                          mainAxisSpacing: 32,
+                          crossAxisSpacing: 32),
+                    ));
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
