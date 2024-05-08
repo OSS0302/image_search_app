20a201693fd8f5323d0f37d5f0c2ff75aa4f6766
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-05-08 14:27:11 +0900

    01 mvvm

--- lib/presentation/pixabay/pxiabay_screen.dart
+++ lib/presentation/pixabay/pxiabay_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:image_search_app/data/model/pixabay_item.dart';
 import 'package:image_search_app/data/repository/pixabay_repository.dart';
 import 'package:image_search_app/data/repository/pixabay_repository_impl.dart';
+import 'package:image_search_app/presentation/pixabay/pixabay_view_model.dart';
 import 'package:image_search_app/presentation/widget/pixabay_widget.dart';
 
 class PixabayScreen extends StatefulWidget {
@@ -15,6 +16,7 @@ class PixabayScreen extends StatefulWidget {
 
 class _PixabayScreenState extends State<PixabayScreen> {
   final imageSearchController = TextEditingController();
+  final pixabayViewModel = PixabayViewModel();
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +42,6 @@ class _PixabayScreenState extends State<PixabayScreen> {
                 enabledBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(20),
                   borderSide: BorderSide(
-                    
                     width: 2,
                     color: Colors.green,
                   ),
@@ -48,7 +49,9 @@ class _PixabayScreenState extends State<PixabayScreen> {
                 hintText: '이미지 앱',
                 suffixIcon: IconButton(
                   icon: Icon(Icons.search_rounded),
-                  onPressed: () {
+                  onPressed: () async {
+                    await pixabayViewModel
+                        .fetchImage(imageSearchController.text);
                     setState(() {});
                   },
                 ),
@@ -57,27 +60,29 @@ class _PixabayScreenState extends State<PixabayScreen> {
             SizedBox(
               height: 24,
             ),
-            FutureBuilder<List<PixabayItem>>(
-              future: PixabayRepositoryImpl()
-                  .getPixabayItems(imageSearchController.text),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                final pixabayItem = snapshot.data!;
-                return Expanded(
+            pixabayViewModel.isLoading
+                ? Center(
+                    child: Column(
+                      children: [
+                        CircularProgressIndicator(),
+                        Text('성공'),
+                      ],
+                    ),
+                  )
+                : Expanded(
                     child: GridView.builder(
-                  itemCount: pixabayItem.length,
-                  itemBuilder: (context, index){
-                    final pixabayItems = pixabayItem[index];
-                    return PixabayWidget(pixabayItems: pixabayItems);
-                  },
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3,crossAxisSpacing: 32,mainAxisSpacing: 32),
-                ));
-              },
-            ),
+                      itemCount: pixabayViewModel.pixabayItem.length,
+                      itemBuilder: (context, index) {
+                        final pixabayItems =
+                            pixabayViewModel.pixabayItem[index];
+                        return PixabayWidget(pixabayItems: pixabayItems);
+                      },
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 32,
+                          mainAxisSpacing: 32),
+                    ),
+                  ),
           ],
         ),
       )),
