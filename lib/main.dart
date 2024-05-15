52a94e684494289fd4f120617b57e0f9fe87d016
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-05-15 17:01:47 +0900

    00 FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:image_search_app/data/repository/image_repository_impl.dart';
-import 'package:image_search_app/di/di_setup.dart';
-import 'package:image_search_app/presentation/image/image_screen.dart';
-import 'package:image_search_app/presentation/image/image_view_model.dart';
-import 'package:image_search_app/routes.dart';
-import 'package:provider/provider.dart';
+import 'package:image_search_app/presentation/pixabay/pixabay_screen.dart';
+
 
 
 void main() {
-  diSetup();
   runApp(const MyApp());
 }
 
@@ -17,13 +12,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
+    return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: PixabayScreen(),
     );
   }
 }
---
+++ lib/presentation/pixabay/pixabay_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:image_search_app/data/model/pixabay_item.dart';
+import 'package:image_search_app/data/repository/pixabay_repository_impl.dart';
+import 'package:image_search_app/presentation/widget/pixabay_widget.dart';
+
+class PixabayScreen extends StatefulWidget {
+  const PixabayScreen({super.key});
+
+  @override
+  State<PixabayScreen> createState() => _PixabayScreenState();
+}
+
+class _PixabayScreenState extends State<PixabayScreen> {
+  final pixabaySearchController = TextEditingController();
+
+  @override
+  void dispose() {
+    pixabaySearchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('이미지 검색앱'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: pixabaySearchController,
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: Colors.amberAccent,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: Colors.amberAccent,
+                    ),
+                  ),
+                  hintText: '이미지를 검색 하세요',
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.search_rounded),
+                    onPressed: () {
+                      setState(() {});
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              FutureBuilder<List<PixabayItem>>(
+                future: PixabayRepositoryImpl()
+                    .getPixabayItems(pixabaySearchController.text),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(
+                      child: Column(
+                        children: [
+                          CircularProgressIndicator(),
+                          Text('잠시만 기다려 주세요'),
+                        ],
+                      ),
+                    );
+                  }
+                  final pixabayItem = snapshot.data!;
+                  return Expanded(
+                    child: GridView.builder(
+                      itemCount: pixabayItem.length,
+                      itemBuilder: (context, index) {
+                        final pixabayItems = pixabayItem[index];
+                        return PixabayWidget(pixabayItems: pixabayItems);
+                      },
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 4,
+                          mainAxisSpacing: 32,
+                          crossAxisSpacing: 32),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
