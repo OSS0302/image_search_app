b6a79aa5ae7789ca906603c7fdec9feae066449e
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-05-15 18:00:37 +0900

    04 mvvm + provider + getIt + router

---
+++ lib/routes.dart
@@ -0,0 +1,11 @@
+import 'package:go_router/go_router.dart';
+import 'package:image_search_app/di/di_setup.dart';
+import 'package:image_search_app/presentation/pixabay/pixabay_screen.dart';
+import 'package:image_search_app/presentation/pixabay/pixabay_view_model.dart';
+import 'package:provider/provider.dart';
+final router = GoRouter(routes: [
+  GoRoute(path: '/',builder: (context , state) => ChangeNotifierProvider(
+    create: (_) => getIt<PixabayViewModel>(),
+    child: PixabayScreen(),
+  ),)
+]);
