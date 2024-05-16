38d497ff58fadd7c69ebb184c86e4ce3b681e148
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-05-16 14:53:10 +0900

    04 mvvm + provider + getIt + router

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:image_search_app/data/repository/image_repository_impl.dart';
 import 'package:image_search_app/presentation/main/main_screen.dart';
 import 'package:image_search_app/presentation/main/main_view_model.dart';
+import 'package:image_search_app/routes.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -13,16 +14,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-    home: ChangeNotifierProvider(
-      create: (_) => MainViewModel(repository: ImageRepositoryImpl()),
-      child: MainScreen(),
-    )
+
     );
   }
 }
