bb69a1ac573d80e976fb6e47475ed4a2e1445e77
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-05-16 14:40:17 +0900

    01 mvvm

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:image_search_app/data/repository/image_repository_impl.dart';
 import 'package:image_search_app/presentation/main/main_view_model.dart';
 import 'package:image_search_app/presentation/widget/image_widget.dart';
 
-import '../../data/model/image_item.dart';
 
 class ImageScreen extends StatefulWidget {
   const ImageScreen({super.key});
@@ -24,10 +22,9 @@ class _ImageScreenState extends State<ImageScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        title: Text('이미지 검색앱'),
+        title: const Text('이미지 검색앱'),
       ),
       body: SafeArea(
           child: Padding(
@@ -39,63 +36,56 @@ class _ImageScreenState extends State<ImageScreen> {
               decoration: InputDecoration(
                 focusedBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide(
+                  borderSide: const BorderSide(
                     width: 2,
                     color: Colors.indigo,
                   ),
                 ),
                 enabledBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide(
+                  borderSide: const BorderSide(
                     width: 2,
                     color: Colors.indigo,
                   ),
                 ),
                 hintText: '이미지 검색앱',
                 suffixIcon: IconButton(
-                  icon: Icon(Icons.search_rounded),
-                  onPressed: () async{
+                  icon: const Icon(Icons.search_rounded),
+                  onPressed: () async {
                     await mainViewModel.fetchImage(imageSearchController.text);
                     setState(() {});
                   },
                 ),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 24,
             ),
-            FutureBuilder<List<ImageItem>>(
-                future: ImageRepositoryImpl()
-                    .getImageItems(imageSearchController.text),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(
-                      child: Column(
-                        children: [
-                          CircularProgressIndicator(),
-                          Text('잠시만 기다려주세요'),
-                        ],
-                      ),
-                    );
-                  }
-                  final imageItem = snapshot.data!;
-                  return Expanded(
-                      child: GridView.builder(
-                    itemCount: mainViewModel.imageItem.length,
-                    itemBuilder: (context, index) {
-                      final imageItems = mainViewModel.imageItem[index];
-                      return ImageWidget(imageItems: imageItems);
-                    },
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4,
-                        mainAxisSpacing: 32,
-                        crossAxisSpacing: 32),
-                  ));
-                }),
+            mainViewModel.isLoading
+                ? const Center(
+                    child: Column(
+                      children: [
+                        CircularProgressIndicator(),
+                        Text('잠시만 기다려주세요'),
+                      ],
+                    ),
+                  )
+                : Expanded(
+                    child: GridView.builder(
+                      itemCount: mainViewModel.imageItem.length,
+                      itemBuilder: (context, index) {
+                        final imageItems = mainViewModel.imageItem[index];
+                        return ImageWidget(imageItems: imageItems);
+                      },
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 4,
+                          mainAxisSpacing: 32,
+                          crossAxisSpacing: 32),
+                    ),
+                  ),
           ],
         ),
       )),
     );
-
   }
 }
