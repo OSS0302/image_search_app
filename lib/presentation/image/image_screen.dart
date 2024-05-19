e6c7ccfa5d246e28da1a7a434bab7c0d78d3afb5
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-05-19 19:53:46 +0900

    08 mvvm + provider + getIt + router + state + result + freezed + showDialog + showsnackBar

--- lib/presentation/image/image_screen.dart
+++ lib/presentation/image/image_screen.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:image_search_app/presentation/image/image_view_model.dart';
 import 'package:image_search_app/presentation/widget/image_widget.dart';
 import 'package:provider/provider.dart';
+
+import 'image_event.dart';
 
 class ImageScreen extends StatefulWidget {
   const ImageScreen({super.key});
@@ -12,11 +17,51 @@ class ImageScreen extends StatefulWidget {
 
 class _ImageScreenState extends State<ImageScreen> {
   final searchController = TextEditingController();
+  StreamSubscription<ImageEvent>? subscription;
+
+  @override
+  void initState() {
+    Future.microtask(() {
+      subscription = context
+          .read<ImageViewModel>()
+          .eventStream
+          .listen((event) {
+        switch (event) {
+          case ShowSnackBar():
+            final snackBar = SnackBar(content: Text(event.message));
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          case ShowDialog():
+            showDialog(context: context, builder: (context) {
+              return AlertDialog(
+                title: Text('이미지 검색앱'),
+                content: Text('이미지 검색앱'),
+                actions: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white
+                    ),
+                    child: TextButton(onPressed: () {
+                      context.pop();
+                    }, child: Text('확인')),
+                  )
+                ],
+              );
+            });
+        }
+      });
+    });
+    super.initState();
+  }
+
+
+
 
 
   @override
   void dispose() {
     searchController.dispose();
+    subscription?.cancel();
     super.dispose();
   }
 
@@ -39,12 +84,12 @@ class _ImageScreenState extends State<ImageScreen> {
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(20),
                     borderSide:
-                        BorderSide(width: 2, color: Colors.purpleAccent),
+                    BorderSide(width: 2, color: Colors.purpleAccent),
                   ),
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(20),
                     borderSide:
-                        BorderSide(width: 2, color: Colors.purpleAccent),
+                    BorderSide(width: 2, color: Colors.purpleAccent),
                   ),
                   hintText: '이미지 검색앱',
                   suffixIcon: IconButton(
@@ -53,7 +98,8 @@ class _ImageScreenState extends State<ImageScreen> {
                       color: Colors.purpleAccent,
                     ),
                     onPressed: () async {
-                     final  result = await imageViewModel.fetchImage(searchController.text);
+                      final result = await imageViewModel.fetchImage(
+                          searchController.text);
 
                       setState(() {});
                     },
@@ -69,7 +115,7 @@ class _ImageScreenState extends State<ImageScreen> {
                   Text('잠시만 기다려 주세요'),
                 ],
               ),)
-             : Expanded(
+                  : Expanded(
                 child: GridView.builder(
                   itemCount: state.imageItem.length,
                   itemBuilder: (context, index) {
--- lib/presentation/image/image_view_model.dart
+++ lib/presentation/image/image_view_model.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:image_search_app/core/result.dart';
 import 'package:image_search_app/data/repository/image_repository_impl.dart';
+import 'package:image_search_app/presentation/image/image_event.dart';
 import 'package:image_search_app/presentation/image/image_state.dart';
 
 import '../../data/model/image_item.dart';
@@ -20,10 +21,15 @@ class ImageViewModel extends ChangeNotifier {
 
   ImageState get state => _state;
 
+  final _eventController = StreamController<ImageEvent>();
+
+  Stream<ImageEvent> get eventStream => _eventController.stream;
+
   Future<void> fetchImage(String query) async {
 
     _state = state.copyWith(isLoading: true);
     notifyListeners();
+
 
     final result = await _repository.getImageItems(query);
    switch(result) {
@@ -31,6 +37,8 @@ class ImageViewModel extends ChangeNotifier {
      case Success<List<ImageItem>>():
        _state = state.copyWith(isLoading: false, imageItem: result.data.toList());
        notifyListeners();
+       _eventController.add(ImageEvent.showSnackBar('성공'));
+       _eventController.add(ImageEvent.showDialog('다이얼로그'));
      case Error<List<ImageItem>>():
        _state.copyWith(isLoading: false);
        notifyListeners();
